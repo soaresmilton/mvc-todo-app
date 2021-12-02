af33c0f90a35dfbf36383670e91ad46e8b7270c0
soaresmilton <eng.milton.soares@gmail.com>
2021-12-02 17:22:22 -0300

    Add Model

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 class Model {
-  constructor() { }
+
+  constructor() {
+    this.todos = [
+      { id: 1, text: 'Caminhada diária', complete: false },
+      { id: 2, text: 'Fazer pelo menos uma aula', complete: false },
+    ]
+  }
+
+
+  addTodo(todoText) {
+    const todo = {
+      id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text: todoText,
+      complete: false,
+    }
+
+    this.todos.push(todo);
+  }
+
+  editTodo(id, updatedText) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id ? { id: todo.id, text: updatedText, complete: todo.complete } : todo
+    );
+  }
+
+  deleteTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+
+  toggleTodo(id) {
+    this.todos = this.todos.map((todo) => todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo);
+  }
+
+
 }
 
 class View {
